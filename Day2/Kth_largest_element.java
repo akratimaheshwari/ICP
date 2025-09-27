098f26417689420ba053b9f5a1008a2b0a3be120
akratimaheshwari <akratimaheshwari634@gmail.in>
2025-09-27 22:31:20 +0530

    Using min Heap

---
+++ Day2/Kth_largest_element.java
@@ -0,0 +1,24 @@
+package Day2;
+
+import java.util.PriorityQueue;
+
+public class Kth_largest_element {
+    public static void main(String[] args){
+        int[] nums = {3,2,1,5,6,4};
+        int k=2;
+        System.out.println(findKthLargest(nums,k));
+    }
+    public static int findKthLargest(int[] nums, int k) {
+        // heap used - largest- minHeap
+        // smallest - max head, use heap window of k size
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.poll();
+    }
+    
+}
